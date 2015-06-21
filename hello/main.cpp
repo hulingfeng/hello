02cceb3f758f3e40b2039c61d24b692e85528adc
hulingfeng <hu_ling_feng@126.com>
2015-06-21 12:45:46 +0800

    update main.cpp

--- hello/main.cpp
+++ hello/main.cpp
@@ -11,6 +11,13 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
-    std::cout << "first commit\n";
+    
+    
+    
+    std::cout << "first commit 哈哈哈\n";
+    
+    
+    
+    
     return 0;
 }
