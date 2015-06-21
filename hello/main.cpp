198f65b7b2cb84bb1ba40df9dbe494da17a66736
hulingfeng <hu_ling_feng@126.com>
2015-06-21 11:57:40 +0800

    first commit

---
+++ hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by hlf on 15/6/21.
+//  Copyright (c) 2015年 hlf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
